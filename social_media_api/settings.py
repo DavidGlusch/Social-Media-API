1150aa52c19e14c04279e02dd5d0b63866a465cf
David Gluschenko <davidglusch@gmail.com>
2023-05-03 20:30:36 +0300

    readme

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-6qdm%m3)i!#suxe$4%in90j+6w*p4#==q6cg6y@bn4d&ju9kwq"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = True
